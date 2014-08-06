317022c78b5be4af3a6b63394f92fd0ac37e6816
doodles92 <daniel.du92@gmail.com>
2014-08-06 08:31:09 -0700

    Financial term value aggregator

---
+++ financial_terms.py
@@ -0,0 +1,42 @@
+import sys
+import csv
+
+word_dic = {}
+
+def extract_data(fname):
+    """Argument takes a csv file name with .csv attachment.
+    return value is a dictionary with all the words and their value attached to it.
+    """
+    with open(fname,'rt') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            word_dic[row[0]]=row[1]
+    return word_dic
+
+def compare_headlines(headlines):
+    """Argument is the list of headlines gathered from stock name.
+    Returns a value number based on comparing thge headline string words
+    with values assigned from word_dic.
+    """
+    total = 0
+    
+    for line in headlines:
+        words = line.split()
+        for word in words:
+            if word in word_dic:
+                total += word_dic[word]
+            else:
+                learning = input("Input value for "+word+ " not found in dictionary: ")
+                word_dic[word] = int(learning)
+                total += int(learning)
+
+
+    return total
+            
+            
+            
+        
+    
+
+        
+    
