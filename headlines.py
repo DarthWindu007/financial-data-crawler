971d02163f15890f9d753e96bd2ae2a1573391a5
Vincent Tai <vtai.7c8@gmail.com>
2014-08-14 08:45:19 -0700

    add Google News to headline sources

--- headlines.py
+++ headlines.py
@@ -10,14 +10,17 @@ def get_headlines(stock_name):
     Argument: stock_name as string, e.g. "YHOO", "GOOG"
     Return value: list of headlines (strings), from the Yahoo RSS feed
     """
-    url="http://finance.yahoo.com/rss/headline?s="+stock_name
-    page=urllib.request.urlopen(url).read().decode()
-    # extract the headlines from the page
-    root=ET.fromstring(page)
-    channel=root[0]
-    headlines=[]
-    for item in channel.findall("item"):
-        headlines.append(item.find("title").text)
+    urls=[]
+    urls.append("http://finance.yahoo.com/rss/headline?s="+stock_name)
+    urls.append("https://news.google.com/news/feeds?hl=en&q="+stock_name+"&output=rss")
+    for url in urls:
+        page=urllib.request.urlopen(url).read().decode()
+        # extract the headlines from the page
+        root=ET.fromstring(page)
+        channel=root[0]
+        headlines=[]
+        for item in channel.findall("item"):
+            headlines.append(item.find("title").text)
     return headlines
 
 def get_score(stock_name):
